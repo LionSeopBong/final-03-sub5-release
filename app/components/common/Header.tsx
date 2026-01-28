8854bb656ded10adf3b9ba035591141b53e5de34
matthewb2 <bestsciencekwabe@gmail.com>
2026-01-28 15:52:00 +0900

    Fix: Image tag

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
@@ -5,19 +6,19 @@ export default function Header() {
     <header className="w-full bg-white-100 border border-0">
       <div className="flex items-center justify-between h-[64px] px-4 min-w-[360px]">
         <div className="text-lg font-bold">
-          <img src="/icons/logo.svg" />
+          <Image src="/icons/logo.svg" alt="" />
         </div>
 
         <div className="flex bg-white-300 items-center gap-8">
           <Link aria-label="weather" href="./weather.html">
-            <img
+            <Image
               src="/icons/arcticons--weather.svg"
               alt="날씨"
               className="w-6 h-6"
             />
           </Link>
           <button aria-label="profile">
-            <img
+            <Image
               src="/icons/healthicons--ui-user-profile-outline.svg"
               alt="프로필"
               className="w-6 h-6"

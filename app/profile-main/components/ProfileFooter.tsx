665d3e7f872dac6a22698c6079ef2711048a97fd
Jia-Oh <ojia7732@gmail.com>
2026-01-30 11:57:25 +0900

    게시판 페이지 컴포넌트화 완료

--- app/profile-main/components/ProfileFooter.tsx
+++ app/profile-main/components/ProfileFooter.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function ProfileFooter() {
   return (
-    <div className="profile-footer fixed bottom-0 left-0 right-0 h-16 flex items-center justify-around gap-2 border border-gray-200 my-4 text-center">
-      <div className="profile-footer-home flex flex-col items-center gap-0.5">
+    <div className="profile-footer fixed bottom-0 left-0 right-0 h-16 flex items-center justify-around gap-2 border border-gray-200 my-4 text-center bg-white">
+      <div className="profile-footer-home flex flex-col items-center gap-0.5 ">
         <Image
           src="/icons/tab-home.svg"
           alt="탭바 홈 아이콘"

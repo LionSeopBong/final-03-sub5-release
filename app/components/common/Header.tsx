03819a6c35c83b8a2c95d126bb089b7d18865ed8
LionSeopBong <gkstjq309@gmail.com>
2026-01-30 10:40:49 +0900

    메인화면 헤더푸터 네비 코드 어셈블

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -6,23 +6,15 @@ export default function Header() {
     <header className="w-full bg-white-100 border border-0">
       <div className="flex items-center justify-between h-[64px] px-4 min-w-[360px]">
         <div className="text-lg font-bold">
-          <Image src="/icons/logo.svg" alt="" />
+          <Image src="/icons/logo.svg" width={100} height={32} priority alt="data" />
         </div>
 
         <div className="flex bg-white-300 items-center gap-8">
           <Link aria-label="weather" href="./weather.html">
-            <Image
-              src="/icons/arcticons--weather.svg"
-              alt="날씨"
-              className="w-6 h-6"
-            />
+            <Image src="/icons/arcticons--weather.svg" alt="날씨" width={100} height={32} priority className="w-6 h-6" />
           </Link>
           <button aria-label="profile">
-            <Image
-              src="/icons/healthicons--ui-user-profile-outline.svg"
-              alt="프로필"
-              className="w-6 h-6"
-            />
+            <Image src="/icons/healthicons--ui-user-profile-outline.svg" width={100} height={32} priority alt="프로필" className="w-6 h-6" />
           </button>
         </div>
       </div>

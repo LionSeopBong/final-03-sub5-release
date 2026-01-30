78d50d16516f908927ba0c8d03af49ec3bf4ed0a
matthewb2 <bestsciencekwabe@gmail.com>
2026-01-30 13:19:50 +0900

    Feature: 프로필 하단 메뉴

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
+import ProfileMenu from "./ProfileMenu"
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full bg-white border-0">
+    <header className="fixed top-0 left-0 w-full bg-white z-50">
       <div className="flex items-center justify-between h-[64px] px-4 min-w-[375px]">
+        {/* 로고 */}
         <div className="text-lg font-bold">
           <Link href="/home">
             <Image
@@ -12,34 +14,26 @@ export default function Header() {
               width={87}
               height={40}
               priority
-              alt="data"
+              alt="logo"
             />
           </Link>
         </div>
 
-        <div className="flex bg-white-300 items-center gap-8">
+        {/* 우측 아이콘 */}
+        <div className="flex items-center gap-8">
           <Link aria-label="weather" href="/weather">
             <Image
               src="/icons/arcticons--weather.svg"
               alt="날씨"
-              width={100}
-              height={32}
+              width={24}
+              height={24}
               priority
-              className="w-6 h-6"
             />
           </Link>
-          <button aria-label="profile">
-            <Image
-              src="/icons/healthicons--ui-user-profile-outline.svg"
-              width={100}
-              height={32}
-              priority
-              alt="프로필"
-              className="w-6 h-6"
-            />
-          </button>
+
+          <ProfileMenu />
         </div>
       </div>
     </header>
-  );
+  )
 }

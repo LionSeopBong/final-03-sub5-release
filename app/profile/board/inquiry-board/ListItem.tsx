6e0a112993ffe92a7e7c4ac54487fbbab765a774
Jia-Oh <ojia7732@gmail.com>
2026-02-04 16:41:46 +0900

    문의게시판 목록 및 공지 상세 페이지 구현중

---
+++ app/profile/board/inquiry-board/ListItem.tsx
@@ -0,0 +1,24 @@
+import { PostListItem } from "@/types/post";
+
+export default function ListItem({ post }: { post: PostListItem }) {
+  return (
+    <li className="border-b border-gray-200 px-2 py-3">
+      <a className="cursor-pointer flex flex-col gap-2 w-full">
+        <div className="flex gap-2">
+          <span className="px-1 text-gray-500">
+            [ {post.repliesCount > 0 ? "답변완료" : "답변대기"} ]
+          </span>
+          <span className="px-1 flex-1 min-w-0 truncate">{post.title}</span>
+        </div>
+        <div className="flex gap-2">
+          <span className="px-1 max-w-[60%] min-w-0 truncate">
+            {post.user.name}
+          </span>
+          <span className="pl-2 border-l-2 border-gray-300 shrink-0">
+            {post.createdAt}
+          </span>
+        </div>
+      </a>
+    </li>
+  );
+}
--- types/post.ts
+++ types/post.ts
@@ -1,6 +1,6 @@
 import type { User } from "@/types/user";
 
-// 댓글 상세
+// 1-a. 댓글 상세
 export interface Reply {
   _id: number;
   content: string;
@@ -9,13 +9,13 @@ export interface Reply {
   updatedAt: string;
 }
 
-// 댓글 생성 폼
+// 1-b. 댓글 생성 폼
 export type ReplyCreateForm = Pick<Reply, "content">;
 
-// 게시글 타입
+// 2-a. 게시글 타입
 export type PostType = "inquiry" | "notice";
 
-// 게시글 상세
+// 2-b. 게시글 상세
 export interface Post {
   _id: number;
   type: PostType;
@@ -28,7 +28,7 @@ export interface Post {
   updatedAt: string;
 }
 
-// 목록용 게시글
+// 2-c. 목록용 게시글
 export type PostListItem = Pick<
   Post,
   "_id" | "type" | "title" | "user" | "views" | "createdAt"
@@ -36,10 +36,19 @@ export type PostListItem = Pick<
   repliesCount: number;
 };
 
-// 게시글 수정 폼
+// 2-d. 게시글 수정 폼
 export type PostUpdateForm = Pick<Post, "title" | "content">;
 
-// 게시글 생성 폼
+// 2-3. 게시글 생성 폼
 export type PostCreateForm = PostUpdateForm & {
   type: PostType;
 };
+
+// 3-a. 공지 상세
+export type Notice = Pick<
+  Post,
+  "_id" | "type" | "title" | "content" | "createdAt"
+>;
+
+// 3-b. 공지 목록
+export type NoticeListItem = Pick<Notice, "_id" | "title" | "createdAt">;

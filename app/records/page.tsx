15035aafeb1bb712f5a2a2da8d9d1c60e7bf8204
LionSeopBong <gkstjq309@gmail.com>
2026-02-10 12:53:09 +0900

    기록 데이터 list csv 파일로 내보내기 기능 적용

--- app/records/page.tsx
+++ app/records/page.tsx
@@ -149,9 +149,32 @@ export default function RecordPage() {
   // 월간 차트
   const monthlyChartData = useMemo(() => getMonthlyDistanceChartData(data), [data]);
 
-  const notYetEvent = () => {
-    alert("미구현!!!");
+  // csv data export
+  const exportData = () => {
+    const headers = "Num,날짜,운동시간,거리(km),페이스,운동타입,장소,칼로리";
+    const sortedData = [...data].sort((a, b) => new Date(b.extra.date).getTime() - new Date(a.extra.date).getTime());
+    const rows = sortedData.map((record, index) => {
+      const date = record.extra.date;
+      const duration = record.extra.duration;
+      const distance = record.extra.distance;
+      const pace = record.extra.pace;
+      const exerciseType = record.extra?.exerciseType || "미입력";
+      const location = record.extra?.location || "미입력";
+      const calories = record.extra?.calories || 0;
+      return `${index + 1},${date},${duration},${distance},${pace},${exerciseType},${location},${calories}`;
+    });
+    const csvString = [headers, ...rows].join("\n");
+    const blob = new Blob(["\uFEFF" + csvString], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "running-record.csv";
+    link.click();
+
+    URL.revokeObjectURL(url);
   };
+
   return (
     <>
       <Header />
@@ -206,7 +229,7 @@ export default function RecordPage() {
         <Link href="/records/all" className="bg-primary text-sm text-white px-5 py-2 active:border-blue-500 rounded-lg">
           전체 기록보기
         </Link>
-        <button className="text-sm border-gray-200 border px-5 py-2 rounded-lg active:border-blue-500" onClick={notYetEvent}>
+        <button className="text-sm border-gray-200 border px-5 py-2 rounded-lg active:border-blue-500" onClick={exportData}>
           내보내기
         </button>
         <Link href="/records/new" className="text-sm border-gray-200 border px-5 py-2 rounded-lg active:border-blue-500">
